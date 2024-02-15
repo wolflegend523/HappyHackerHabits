3a131602f37be1c27e155855517639c3a725791d
wolflegend523 <cordelia.notbohm@outlook.com>
2024-02-14 21:29:45 -0800

    simplify env variables to simplify managing local vs render development

--- frontend/src/api/debuggingDuckApi.js
+++ frontend/src/api/debuggingDuckApi.js
@@ -1,7 +1,4 @@
-const backendHostname = process.env.REACT_APP_BACKEND_HOSTNAME;
-const backendPort = process.env.REACT_APP_BACKEND_PORT;
-const backendBaseApi = process.env.REACT_APP_BACKEND_API_BASE_URL;
-const apiEndpoint = `http://${backendHostname}:${backendPort}/${backendBaseApi}`;
+const apiEndpoint = process.env.REACT_APP_BACKEND_API_BASE_URL;
 
 async function getDebuggingDuck() {
   const response = await fetch(`${apiEndpoint}debuggingduck/`);
