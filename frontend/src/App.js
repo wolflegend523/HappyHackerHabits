1f6e6ce85e6a4beec029e94ece9d00bda65d2f27
wolflegend523 <cordelia.notbohm@outlook.com>
2024-02-15 11:43:42 -0800

    clean up app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,16 +8,18 @@ function App() {
   const [duckMessage, setDuckMessage] = useState(null);
 
   useEffect(() => {
-    getDebuggingDuck().then((duck) => setDuckMessage(duck.message));
+    async function fetchDuck() {
+      const duck = await getDebuggingDuck();
+      setDuckMessage(duck.message);
+    }
+
+    fetchDuck();
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         {duckMessage && <p>{duckMessage}</p>}
       </header>
     </div>
