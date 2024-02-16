d68a607cd88031f5e4f4cbc5a285835b11e09ef9
wolflegend523 <cordelia.notbohm@outlook.com>
2024-02-15 23:38:51 -0800

    create backend daily quote API

--- backend/api/quoteApi.js
+++ backend/api/quoteApi.js
@@ -5,22 +5,89 @@ const { authorizeToken } = require("../authToken");
 // prisma client to interact with the database
 const prisma = new PrismaClient();
 
+// API endpoint to get a user's daily quote
+const quotableAPIEndpoint = "https://api.quotable.io/random";
+const technologyTag = "technology";
+
 
 /**
- * TODO: get a user's daily quote
+ * get a user's daily quote
  */
+router.get("/daily/", async (req, res) => {
+  // Check if the user has a valid token
+  if (!authorizeToken(req, res)) {
+    return;
+  }
+
+  try {
+    // check if there is a quote in the database for today
+    const today = new Date();
+    const todayNoTime = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    const dbQuote = await prisma.quote.findFirst({
+      select: {
+        quote_id: true,
+        quote_author: true,
+        quote_text: true,
+      },
+      where: {
+        generated_at: todayNoTime,
+      },
+    });
+
+    // if there is a quote for today, send it back
+    if (dbQuote) {
+      res.json(dbQuote);
+      return;
+    } 
+
+    // if there is not already a quote for today get a random quote from the quotable API
+    const response = await fetch(quotableAPIEndpoint + "?tags=" + technologyTag);
+    if (!response.ok) {
+      res.status(500).json({error: "Failed to generate a quote"});
+    }
+    const newQuote = await response.json();
+
+    // save the quote in the database
+    const newDbQuote = await prisma.quote.create({
+      data: {
+        quote_author: newQuote.author,
+        quote_text: newQuote.content,
+        generated_at: todayNoTime,
+      },
+      select: {
+        quote_id: true,
+        quote_author: true,
+        quote_text: true,
+      },
+    });
+
+    // send the new quote 
+    res.json(newDbQuote);
+  } catch (err) {
+    res.status(400).json(err); // Error when creating the quote
+  }
+});
 
 
 /**
  * TODO: get a user's favorite quotes
  */
+router.get("/favorites/", async (req, res) => {
+});
 
 
 /**
  * TODO: post to user's favorite quotes
  */
+router.post("/favorites/", async (req, res) => {
+});
 
 
 /**
  * TODO: delete from the user's favorite quotes
  */
+router.delete("/favorites/", async (req, res) => {
+});
+
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use('/api/users/:userId', (req, res, next) => {
 // Create API endpoints
 app.use("/api/debuggingduck", require("./api/debuggingDuckApi"));
 app.use("/api/users/:userId/goals", require("./api/goalApi"));
-//app.use("/api/users/:userId/quotes", require("./api/quoteApi"));
+app.use("/api/users/:userId/quotes", require("./api/quoteApi"));
 app.use("/api/users", require("./api/userApi"));
 
 // Start up the server
