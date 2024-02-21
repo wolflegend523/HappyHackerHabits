45e1ad220db72171db1bc40a6041735220c4a6b3
wolflegend523 <cordelia.notbohm@outlook.com>
2024-02-20 16:32:38 -0800

    change frontend titlebar depending on if a user is logged in

--- frontend/src/components/Titlebar.jsx
+++ frontend/src/components/Titlebar.jsx
@@ -1,8 +1,14 @@
 import { Link } from 'react-router-dom';
 import logo from '../icons/happyHackerLogoColor.svg';
 import styles from '../styles/Titlebar.module.css';
+import {useSelector} from 'react-redux'
+
 
 const Titlebar = () => {
+  const { userIsLoggedIn, userProfile } = useSelector(
+    (state) => state.user
+  );
+
   return (
     <header className={styles.titlebar}>
       <img src={logo} alt="logo" className={styles.icon} />
@@ -10,8 +16,10 @@ const Titlebar = () => {
       <Link className={styles.title} to="/">Happy Hacker Habits</Link>
       </div>
       <div className={styles.titlebarButtons}>
-        <Link className={styles.titlebarButton} to="/login/">Login</Link>
-        <Link className={styles.titlebarButton} to="/signup/">Signup</Link>
+        {userIsLoggedIn && <Link className={styles.titlebarButton} to="/profile/">{userProfile.display_name}</Link>}
+        {userIsLoggedIn &&<Link className={styles.titlebarButton}>Setting</Link>}
+        {!userIsLoggedIn && <Link className={styles.titlebarButton} to="/login/">Login</Link>}
+        {!userIsLoggedIn && <Link className={styles.titlebarButton} to="/signup/">Signup</Link>}
       </div>
     </header>
   );
